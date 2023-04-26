9df93a3ae6a0ef6c2b3b9968b663084d86d769cd
makuusik <makunka44@gmail.com>
2023-04-26 03:47:22 +0300

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -15,4 +15,24 @@ const swiper = new Swiper('.mySwiper', {
   },
 });
 
-
+// const swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   keyboard: {
+//     enabled: true,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     renderBullet: function (index, className) {
+//       if (index === 0 || index === swiper.slides.length - 1) {
+//         return '<span class="' + className + '">' + (index + 1) + '</span>';
+//       }
+//       return '';
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.next-slide',
+//     prevEl: '.prev-slide',
+//   },
+// });
